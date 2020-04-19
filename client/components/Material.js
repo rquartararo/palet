93b7a5e05ce2311a9446d7826bd92169a6a88992
Wenfei Zhou <wzhou2046@gmail.com>
2020-04-19 17:07:48 -0400

    last commit

--- client/components/Material.js
+++ client/components/Material.js
@@ -1,16 +1,45 @@
-import React, { Component, Fragment, useState, useEffect } from "react";
+import React, { Component, Fragment, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import postArr from './mockData'
+import postArr from './mockData';
 
-
-const Material = ({ material }) => {
-  console.log('i am here')
-
+const Material = ({ item }) => {
+  console.log(item);
   return (
-    < div >
-      <h1>material</h1>
-    </div >
-  )
+    <div>
+      <div className='container'>
+        <h2>This is made by {item.materialName}</h2>
+        <p>{item.materialName}</p>
+        <p>{item.materialType}</p>
+      </div>
+    </div>
+  );
+};
+
+class List extends React.Component {
+  render() {
+    const friends = this.props.friends;
+
+    return (
+      <ul>
+        {friends.map((friend) => (
+          <li></li>
+        ))}
+      </ul>
+    );
+  }
 }
+
+ReactDOM.render(
+  <List
+    friends={[
+      { id: 893, name: 'Mikenzi' },
+      { id: 871, name: 'Cash' },
+      { id: 982, name: 'Steven' },
+      { id: 061, name: 'Kimmy' },
+      { id: 117, name: 'Doug' },
+    ]}
+  />,
+  document.getElementById('app')
+);
 
 export default Material;
