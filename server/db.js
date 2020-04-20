ee91697a5f61641cff7ce387ef68006a61740bed
Wenfei Zhou <wzhou2046@gmail.com>
2020-04-20 00:35:36 -0400

    fixed the server.js file

--- server/db.js
+++ server/db.js
@@ -2,15 +2,16 @@
 
 const Pool = require('pg').Pool;
 
-const uri = 'postgres://buutetfr:IRII6Wgus0ucpg-jlJi6QdKDdHvimaqc@drona.db.elephantsql.com:5432/buutetfr';
+const uri =
+  'postgres://buutetfr:IRII6Wgus0ucpg-jlJi6QdKDdHvimaqc@drona.db.elephantsql.com:5432/buutetfr';
 
 const pool = new Pool({
   user: 'jimmyphong',
   password: '4500',
   host: 'localhost',
   port: 5432,
-  database: 'palet'
-})
+  database: 'palet',
+});
 
 const poolRemote = new Pool({
   connectionString: uri,
