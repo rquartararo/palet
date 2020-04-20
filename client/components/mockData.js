9affce0e867a0940ccbe427aad3c1ac7c01a7ad2
Wenfei Zhou <wzhou2046@gmail.com>
2020-04-19 23:46:38 -0400

    commit missing files

--- client/components/mockData.js
+++ client/components/mockData.js
@@ -2,8 +2,6 @@ const postArr = [
   {
     postId: 'rachel1',
     name: 'Rachel',
-    process:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non laudantium dolore quisquam, autem commodi vel exercitationem, itaque neque libero vitae quas accusamus sint totam, sequi culpa ullam hic rerum aliquam sit perspiciatis soluta. Error velit provident reiciendis, ipsum repellat optio',
     url:
       'https://i.pinimg.com/originals/a2/47/8f/a2478ff279ba9d34aa93a4ac5f119d22.jpg',
     materialsUsed: [
@@ -17,8 +15,6 @@ const postArr = [
   {
     postId: 'wenfei2',
     name: 'Wenfei',
-    process:
-      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non laudantium dolore quisquam, autem commodi vel exercitationem, itaque neque libero vitae quas accusamus sint totam, sequi culpa ullam hic rerum aliquam sit perspiciatis soluta. Error velit provident reiciendis, ipsum repellat optio',
     url:
       'https://i.pinimg.com/originals/a2/47/8f/a2478ff279ba9d34aa93a4ac5f119d22.jpg',
     materialsUsed: [
